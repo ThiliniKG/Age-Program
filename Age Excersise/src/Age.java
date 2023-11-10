ae5c11b724140c94b40590af5cf9d924b5434ff1
Thilini Gamage <thilinifin@outlook.comexample.com>
2023-11-10 15:35:21 +0200

    Age Program

---
+++ Age Excersise/src/Age.java
@@ -0,0 +1,150 @@
+
+public class Age {
+
+	public static void main(String[] args) {
+		
+//		int Age = 18;
+//		
+//	//  Print "You are underage.",if age is less than 18	
+//		
+//		if (Age > 0 && Age < 18)
+//		{	
+//			System.out.println("You are underage.");
+//
+//	}
+//		else
+//		{	
+//			System.out.println("You are an adult.");
+//
+//	}	
+//			
+//		 System.out.println("Press space to exit."); 
+//		 
+		
+//		int Age =5;
+//		
+//		if (Age >0 && Age<18)
+//	{		
+//		System.out.println("You are underage.");
+//		
+//	}
+//	
+//		else
+//		{	
+//			
+//			System.out.println("You are an adult.");
+//		
+//		}	
+//		
+//
+//			
+//			System.out.println("Press space to exit.");
+//			
+//			if (Age >0 && Age<18)
+//			{		
+//				System.out.println("You are underage.");
+//				
+//			}
+//			
+//			else if (Age >=65)
+//			{	
+//					
+//				System.out.println("You are retired.");
+//				
+//			}	
+//				
+//			else 
+//				
+//			{			
+//			System.out.println("You are an adult.");		
+//			
+//	        }
+		
+//		int Age =10;
+//		
+//		if (Age >0 && Age<18)
+//			{		
+//			System.out.println("You are underage.");
+//				
+//		}
+//		
+//		if (Age >=15)
+//		{		
+//			System.out.println("You can drive a moped.");
+//				
+//		}	
+//		
+//		else if (Age >=65)
+//		{		
+//			System.out.println("You are retired.");
+//				
+//		}	
+//		else
+//		{		
+//			System.out.println("You are an adult.");
+//				
+//		}		
+			
+//		int Age =20;
+//		
+//		if (Age > 0 && Age < 18) 
+//		
+//		{ 
+//			System.out.println("You are underage."); 
+//		}
+//		
+//		
+//		
+//		if (Age >=18)
+//			{		
+//			System.out.println("You can drive a car.");
+//		
+//			}	
+		
+		int Age =40;
+				
+				if (Age==10)
+					
+				{ System.out.println("Anniversary Party!!.");
+		
+				}
+                if (Age==20)
+					
+				{ System.out.println("Anniversary Party!!.");
+		
+				}
+                if (Age==30)
+					
+				{ System.out.println("Anniversary Party!!.");
+		
+				}
+		
+                if (Age==40)
+					
+				{ System.out.println("Anniversary Party!!.");
+		
+				}
+                
+               if (Age==50)
+	
+                { System.out.println("Anniversary Party!!.");
+
+}
+               if (Age==100)
+            		
+               { System.out.println(" congratulations!."
+               		+ " congratulations! ."
+               		+ " congratulations! ");
+
+}
+                
+               if (Age >=40 && Age <=50)
+           		
+               { System.out.println("happy mid-life.");
+                
+                
+               }   
+                
+                
+}
+}
